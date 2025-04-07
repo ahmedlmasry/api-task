d12f1e56d128741227d219db4167c2d86b26dd6e
ahmedlmasry <elmasryahmed162@gmail.com>
2025-04-08 01:55:41 +0200

    upload project

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Models\Post;
+use Illuminate\Support\Facades\Gate;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::with('user')->get();
+        return $posts;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+    {
+        $post = $request->user()->posts()->create($request->validated());
+        return response()->json(['message'=>'post created successfully','post'=>$post]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return $post->load('user');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        Gate::authorize('modify', $post);
+        $post->update($request->validated());
+        return $post;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        Gate::authorize('modify', $post);
+        $post->delete();
+        return ['message' => 'Post deleted successfully'];
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/user',[AuthController::class, 'user']);
+    Route::get('logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('posts', PostController::class);
+});
